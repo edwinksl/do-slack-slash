c5050da8a6edeb77ba79c7b49c4fe2ca4e9cd2c9
Edwin Khoo <edwinksl@gmail.com>
2017-08-10 07:42:26 -0400

    Add Flask app for Slack slash command

---
+++ myproject.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import dotenv
+import os
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+dotenv.load_dotenv(dotenv_path)
+verification_token = os.environ['VERIFICATION_TOKEN']
+
+
+@app.route('/slash')
+def slash():
+    if request.form['token'] == verification_token:
+        payload = {'text': 'DigitalOcean Slack slash command is successful!'}
+        return jsonify(payload)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
